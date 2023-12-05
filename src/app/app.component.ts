70a0c628fad6a53208ad50298147afce05ff596c
Jefferson Ferreira <jefferson14489@gmail.com>
2023-12-05 16:24:17 -0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MenuTitleComponent,
+    BigCardComponent,
+    MenuBarComponent,
+    SmallCardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
